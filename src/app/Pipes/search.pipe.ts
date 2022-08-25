bdd08c9c93b0a5dec694b4d5960b3b83d25e024c
robert-devs <mainrobert04@gmail.com>
2022-08-25 10:56:06 +0300

    modifing the search pipe

--- src/app/Pipes/search.pipe.ts
+++ src/app/Pipes/search.pipe.ts
@@ -7,12 +7,15 @@ import { Student } from '../inaterface/studentInterfaces';
 export class searchPipe implements PipeTransform {
   transform(value: Student[], name: string): Student[] {
     if (value.length == 0 || name == '') {
-      name = name.toLowerCase();
+      // name = name.toLowerCase();
       return value;
     }
     const searched: Student[] = [];
     for (let students of value) {
-      if (students.name.toLowerCase().indexOf(name.toLocaleLowerCase()) != -1) {
+      if (
+        students.name.toLocaleLowerCase().indexOf(name.toLocaleLowerCase()) !==
+        -1
+      ) {
         searched.push(students);
       }
     }
--- src/app/components/all-student/all-student.component.ts
+++ src/app/components/all-student/all-student.component.ts
@@ -8,13 +8,31 @@ import { StudentsService } from 'src/app/Services/Students.service';
   styleUrls: ['./all-student.component.scss'],
 })
 export class AllStudentComponent implements OnInit {
-  students!: Student[];
-
-  search = '';
+  students: Student[] = [];
+  newStude: Student[] = [];
+  private _listFiter: string = '';
+  filteredStudents: Student[] = [];
 
   constructor(private studentsService: StudentsService) {}
 
+  get listFilter(): string {
+    return this._listFiter;
+  }
+
+  set listFilter(value: string) {
+    this._listFiter = value;
+    this.filteredStudents = this.performFilter(value);
+  }
+
+  performFilter(filterBy: string) {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.students.filter((student) =>
+      student.name.toLocaleLowerCase().includes(filterBy)
+    );
+  }
+
   ngOnInit(): void {
     this.students = this.studentsService.getStudent();
+    this.filteredStudents = this.students;
   }
 }
